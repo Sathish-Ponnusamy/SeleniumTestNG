4f29a0d8ecde25ec05f99631e4fbfdbca3e87e17
SPL <spl@spls-mbp.home>
2025-11-30 10:33:57 +0000

    SoftAssert and HardAssert

--- src/test/java/retryAnalyser/MyTests.java
+++ src/test/java/retryAnalyser/MyTests.java
@@ -2,21 +2,27 @@ package retryAnalyser;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class MyTests {
+
+    SoftAssert softassert = new SoftAssert();
 
     @Test(retryAnalyzer = utils.RetryAnalyzer.class)
     public void Test()
     {
-        Assert.assertEquals(false, true);
+//        Assert.assertEquals(false, true);
+        softassert.assertEquals(false, true);
+        softassert.assertAll();
 
     }
 
     @Test(retryAnalyzer = utils.RetryAnalyzer.class)
     public void Test1()
     {
-        Assert.assertEquals(true,false );
-
+//        Assert.assertEquals(true,false );
+        softassert.assertEquals(true,false );
+        softassert.assertAll();
     }
 
 }
--- src/test/java/retryAnalyser/MyTestsRetryAnalyzerTransformerTests.java
+++ src/test/java/retryAnalyser/MyTestsRetryAnalyzerTransformerTests.java
@@ -2,21 +2,26 @@ package retryAnalyser;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class MyTestsRetryAnalyzerTransformerTests {
 
+    SoftAssert softAssert = new SoftAssert();
     @Test()
     public void Test()
     {
-        Assert.assertEquals(false, true);
+//        Assert.assertEquals(false, true);
+        softAssert.assertEquals(false, true);
+        softAssert.assertAll();
 
     }
 
     @Test()
     public void Test1()
     {
-        Assert.assertEquals(true,false );
-
+//        Assert.assertEquals(true,false );
+softAssert.assertEquals(true,false);
+        softAssert.assertAll();
     }
 
     @Test
@@ -24,5 +29,7 @@ public class MyTestsRetryAnalyzerTransformerTests {
     {
         Assert.assertEquals(true,true );
     }
+
+
 
 }
